5ba4e52babc42d6a884450624305df8cb01e7138
Adrian Andre <adrianadero.otieno@gmail.com>
2017-04-05 12:25:11 +0300

    Update of Word_Count Function,py
    Added several lines of code to give it functionality

--- Andelabs Day 3/Word_count/Word_Count Function.py
+++ Andelabs Day 3/Word_count/Word_Count Function.py
@@ -1,1 +1,17 @@
 def words(sentence):
+    if isinstance(sentence, str):
+        result_dict = {}
+        i = 0
+        split_sentence = sentence.split()
+        for i in range(i, len(split_sentence)):
+            print(result_dict)
+            for j in range(i+1, len(split_sentence)):
+                if split_sentence[i] == split_sentence[j]:
+                    print(split_sentence[i])
+                    result_dict[split_sentence[i]] = 1
+                    result_dict[split_sentence[i]] += 1
+                #else:
+                    #result_dict[split_sentence[i]] = 1
+
+
+    return (result_dict)
