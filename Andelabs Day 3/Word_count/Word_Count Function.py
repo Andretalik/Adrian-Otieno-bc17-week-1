7e6b935ea25a0ec2d8fb2785799a8cadcc6cb3ba
Adrian Andre <adrianadero.otieno@gmail.com>
2017-04-05 14:22:55 +0300

    Update of Word_Count.py
    Latest update to change logical flow to allow the code to pass all tests written beforehand

--- Andelabs Day 3/Word_count/Word_Count Function.py
+++ Andelabs Day 3/Word_count/Word_Count Function.py
@@ -1,11 +1,19 @@
 def words(sentence):
     if isinstance(sentence, str):
         split_sentence = sentence.split()
-        '''for word in split_sentence:
-            if word.isdigit() in split_sentence:
-                word = int(word)'''
-        result_dict = {i: split_sentence.count(i) for i in split_sentence}
-        return result_dict
-
-    else:
-        return "Only sentences allowed"
+        result_dict = {}
+        for word in split_sentence:
+            #print(word) : this was and still could be used for debugging purposes
+            if word in result_dict:
+                if word.isdigit():
+                    word = int(word)
+                    result_dict[word] += 1
+                else:
+                    result_dict[word] += 1
+            else:
+                if word.isdigit():
+                    word = int(word)
+                    result_dict[word] = 1
+                else:
+                    result_dict[word] = 1
+    return result_dict
