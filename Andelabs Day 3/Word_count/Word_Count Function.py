6d07ad6b9859280f301dd3dd74cbe4efffd191b3
Adrian Andre <adrianadero.otieno@gmail.com>
2017-04-05 14:11:32 +0300

    Updated Word_Count.py
    this code fails the final test simply due to the quotation marks making the int values get returned as strings
    otherwise it is correct

--- Andelabs Day 3/Word_count/Word_Count Function.py
+++ Andelabs Day 3/Word_count/Word_Count Function.py
@@ -1,17 +1,11 @@
 def words(sentence):
     if isinstance(sentence, str):
-        result_dict = {}
-        i = 0
         split_sentence = sentence.split()
-        for i in range(i, len(split_sentence)):
-            print(result_dict)
-            for j in range(i+1, len(split_sentence)):
-                if split_sentence[i] == split_sentence[j]:
-                    print(split_sentence[i])
-                    result_dict[split_sentence[i]] = 1
-                    result_dict[split_sentence[i]] += 1
-                #else:
-                    #result_dict[split_sentence[i]] = 1
+        '''for word in split_sentence:
+            if word.isdigit() in split_sentence:
+                word = int(word)'''
+        result_dict = {i: split_sentence.count(i) for i in split_sentence}
+        return result_dict
 
-
-    return (result_dict)
+    else:
+        return "Only sentences allowed"
