f30e0deb901cf70ae99b1f427867d041fc90cf23
Adrian Andre <adrianadero.otieno@gmail.com>
2017-04-05 17:19:10 +0300

    Final Working Update
    Fixed issue of numbers not being counted as more than 1 even when they are more than 1

--- Andelabs Day 3/Word_count/Word_Count Function.py
+++ Andelabs Day 3/Word_count/Word_Count Function.py
@@ -1,11 +1,24 @@
 def words(sentence):
     if isinstance(sentence, str):
         split_sentence = sentence.split()
-        '''for word in split_sentence:
-            if word.isdigit() in split_sentence:
-                word = int(word)'''
-        result_dict = {i: split_sentence.count(i) for i in split_sentence}
-        return result_dict
+        result_dict = {}
+        for word in split_sentence:
+            # print(word) : this was and still could be used for debugging purposes
 
-    else:
-        return "Only sentences allowed"
+            if word.isdigit():
+                if int(word) in result_dict:
+                    result_dict[int(word)] += 1
+                    continue
+            if word in result_dict:
+                if word.isdigit():
+                    word = int(word)
+                    result_dict[word] += 1
+                else:
+                    result_dict[word] += 1
+            else:
+                if word.isdigit():
+                    word = int(word)
+                    result_dict[word] = 1
+                else:
+                    result_dict[word] = 1
+    return result_dict
