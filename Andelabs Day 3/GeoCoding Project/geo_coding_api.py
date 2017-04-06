bb9401b4eb815abe8846806e772cac503241bb40
Adrian Andre <adrianadero.otieno@gmail.com>
2017-04-06 07:25:01 +0300

    Removed Comments
    Removed the code that was commented out fro debugging processes while writing the code

--- Andelabs Day 3/GeoCoding Project/geo_coding_api.py
+++ Andelabs Day 3/GeoCoding Project/geo_coding_api.py
@@ -1,8 +1,7 @@
 import requests
 
-# https://maps.googleapis.com/maps/api/geocode/json?address=nairobi&key=AIzaSyBP46knHGvg5jxJkrgsGLUdwd5DzdTltno
-google_api = 'key=AIzaSyBP46knHGvg5jxJkrgsGLUdwd5DzdTltno'
 
+google_api = 'key=AIzaSyBP46knHGvg5jxJkrgsGLUdwd5DzdTltno'
 
 def geocode_search(term):
     api_key = google_api
@@ -11,8 +10,4 @@ def geocode_search(term):
     final_url = url + "address=" + address_term + "&" + api_key
     json_object = requests.get(final_url)
     data = json_object.json()
-    # decoded_data = json.load(data)
-    # print(data["results"][0]["geometry"]["location"].keys())
-    # print(len(data["results"]))
-
     print("Latitude: ", data["results"][0]["geometry"]["location"]["lat"], ", ","Longitude", data["results"][0]["geometry"]["location"]["lng"] )
